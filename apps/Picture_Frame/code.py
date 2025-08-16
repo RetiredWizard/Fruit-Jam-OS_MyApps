357b0ccb66dfe7127ba1e3fb67bf44ffc9d8613b
RetiredWizard <github@retiredwizard.com>
2025-08-15 22:46:12 -0400

    Display time clearification

--- apps/Picture_Frame/code.py
+++ apps/Picture_Frame/code.py
@@ -3,9 +3,10 @@ import playimage
 
 # This will play all images (BMP, JPG, PNG, RLE, GIF) in the current directory
 # or in the /sd/ directory if no images are found in the current directory.
-# It will display each image for 20 seconds.
 #
 # Note that GIF animations must have a width < = 320.
 
+# The 20 at the end of the file list is the number of seconds to display each
+# image. To change the display time, change the number after the comma.
 playimage.playimage("*.*,20")
 supervisor.reload()
